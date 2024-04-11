2119aeaa9dbdc2480652279019f11a298cc461b0
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-11 23:07:36 +0200

    initial commit

---
+++ src/main/java/com/library/Sociallibrary/SociallibraryApplication.java
@@ -0,0 +1,13 @@
+package com.library.Sociallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociallibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociallibraryApplication.class, args);
+	}
+
+}
