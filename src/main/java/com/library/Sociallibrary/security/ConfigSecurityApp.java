b606f6f30d999436c5c5dfa0af7c190f8cd16f78
jocelin kisenga <kisengajocelin7@gmail.com>
2024-04-14 11:09:00 +0200

    added security register configuration

---
+++ src/main/java/com/library/Sociallibrary/security/ConfigSecurityApp.java
@@ -0,0 +1,27 @@
+package com.library.Sociallibrary.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurityApp {
+
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize -> authorize.requestMatchers(HttpMethod.POST, "/register")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .build();
+    }
+}
